1f4a0053ec9885d76125700b5f015ad443d1a41c
Zakaria Bahhah <eng.zaky@gmail.com>
2018-04-01 16:43:22 +0300

    TribleReverse

--- hello/hello.go
+++ hello/hello.go
@@ -7,6 +7,7 @@ import (
 )
 
 func main() {
-	fmt.Printf(stringutil.Reverse("\n\n!oG ,olleH\n"))
+	//fmt.Printf(stringutil.Reverse("\n\n!oG ,olleH\n"))
 	//fmt.Printf(stringutil.ReverseReverse("\n\nHello Go!!\n\n"))
+	fmt.Printf(stringutil.TribleReverse("\n\nHello Go!!\n\n"))
 }
