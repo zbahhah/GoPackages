42771861bafdb412cc79ea7dd3a0c9c1b3a75060
Zakaria Bahhah <eng.zaky@gmail.com>
2018-04-01 16:21:21 +0300

    create hello.go file

---
+++ hello/hello.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/zbahhah/GoPackages/stringutil"
+)
+
+func main() {
+	fmt.Printf(stringutil.Reverse("\n\n!oG ,olleH\n"))
+	//fmt.Printf(stringutil.ReverseReverse("\n\nHello Go!!\n\n"))
+}
